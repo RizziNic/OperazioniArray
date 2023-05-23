29bc3451c89af85a68e7b07855b89341e1c9e574
RizziNic <rizzijunior.nico@gmail.com>
2023-05-23 10:50:18 +0200

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try{
+            FileReader i = new FileReader("input.txt");
+            FileWriter o = new FileWriter("output.txt");
+
+            BufferedReader reader= new BufferedReader(i);
+            BufferedWriter writer = new BufferedWriter(o);
+
+            int n = Integer.parseInt(reader.readLine());
+
+            String[] scrittura= reader.readLine().split(" ");
+            int somma = 0;
+
+            for (int j=0;j< scrittura.length;j++){
+                somma = somma + Integer.parseInt(scrittura[j]);
+            }
+
+            float media=0;
+            media= (float)somma/n;
+            writer.write(somma + " " + media);
+            writer.flush();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
+
